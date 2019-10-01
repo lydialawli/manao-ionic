eff95e53deb57dd9bc65bc04ead2d0385bb1197e
Ly <lydialawli@gmail.com>
2019-10-01 16:46:37 +0700

    playMenu yellow box

--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -1,4 +1,4 @@
-import { IonButtons, IonContent, IonHeader, IonIcon, IonItem, IonList, IonMenuButton, IonPage, IonGrid, IonColumn, IonRow, IonTitle, IonToolbar, IonText } from '@ionic/react';
+import { IonButtons, IonContent, IonHeader, IonIcon, IonItem, IonList, IonMenuButton, IonPage, IonGrid, IonCol, IonRow, IonTitle, IonToolbar, IonText } from '@ionic/react';
 import { flask, football, paperPlane, wifi, lock } from 'ionicons/icons';
 import React from 'react';
 import '../styles/quiz.css';
@@ -8,37 +8,41 @@ class Quiz extends React.Component {
     render() {
         return (
             <IonPage>
-                {/* <IonButtons slot="start">
-                    <IonMenuButton />
-                </IonButtons>
-                <IonHeader className="playMenu"  attribute="translucent">
-          <IonToolbar className="playMenu">
-            <IonButtons slot="start">
-              <IonMenuButton />
-            </IonButtons>
-          </IonToolbar>
-        </IonHeader> */}
+                <IonHeader>
+                    <IonToolbar className="quizbar">
+                        <IonButtons slot="start">
+                            <IonMenuButton />
+                        </IonButtons>
+                        <div className="yellowBox"></div>
+                    </IonToolbar>{" "}
+                </IonHeader>{" "}
 
                 <IonContent className="quizMain  ion-padding">
                     <IonGrid>
                         <IonRow>
-                            <h1 className="challengeNum">CHALLENGE # 1</h1>
+                            <h1 className="titleChallenge">CHALLENGE # 1</h1>
                         </IonRow>
-                        <IonRow className="extraInfo">
+                        <div className="extraInfo">
                             <h6>Extra info</h6>
                             <p className="description" >
                                 Markets are a huge part of the Thai culture, and the locals love markets just as much as tourists.
                     This exact spot on a Sunday evening is awesome!
                         </p>
-                        </IonRow>
+                        </div>
 
                     </IonGrid>
                     <IonGrid>
-                        <IonIcon className="manaoLogo" src="assets/Logo.svg"></IonIcon>
-                        <p className="problem" >
-                            Wow, so hot in here! How do this people survive?
-                        </p>
+                        <IonRow>
+                            <IonCol>
+                                <IonIcon className="manaoLogo" src="assets/Logo.svg"></IonIcon>
+                            </IonCol>
 
+                            <IonCol>
+                                <p className="problem" >
+                                    Wow, so hot in here! How do this people survive?
+                        </p>
+                            </IonCol>
+                        </IonRow>
                     </IonGrid>
                     <div className="triangle"></div>
                     <div className="footerQuiz ">
