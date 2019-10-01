0c76164a997b0e3ad9c6eb861b275ba46466a83c
Ly <lydialawli@gmail.com>
2019-10-01 13:30:32 +0700

    extra info added

--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -1,33 +1,36 @@
-import { IonButtons, IonContent, IonHeader, IonIcon, IonItem, IonList, IonMenuButton, IonPage, IonTitle, IonToolbar,IonText } from '@ionic/react';
+import { IonButtons, IonContent, IonHeader, IonIcon, IonItem, IonList, IonMenuButton, IonPage, IonTitle, IonToolbar, IonText } from '@ionic/react';
 import { flask, football, paperPlane, wifi } from 'ionicons/icons';
 import React from 'react';
 import '../styles/quiz.css';
 
 
 class Quiz extends React.Component {
-  render() {
-    return (
-      <IonPage>
-        <IonHeader attribute="translucent">
+    render() {
+        return (
+            <IonPage>
+                {/* <IonButtons slot="start">
+                    <IonMenuButton />
+                </IonButtons>
+                <IonHeader className="playMenu"  attribute="translucent">
           <IonToolbar className="playMenu">
             <IonButtons slot="start">
               <IonMenuButton />
             </IonButtons>
           </IonToolbar>
-        </IonHeader>
+        </IonHeader> */}
 
-        <IonContent className="quizMain">
-          <IonText className="description">
-              <h6>Extra info</h6>
-            <p>
-            Markets are a huge part of the Thai culture, and the locals love markets just as much as tourists. 
-This exact spot on a Sunday evening is awesome!
-            </p>
-          </IonText>
-        </IonContent>
-      </IonPage >
-    )
-  }
+                <IonContent className="quizMain  ion-padding">
+                    <div className="extraInfo">
+                        <h6>Extra info</h6>
+                        <p  className="description" >
+                            Markets are a huge part of the Thai culture, and the locals love markets just as much as tourists.
+                This exact spot on a Sunday evening is awesome!
+                        </p>
+                    </div>
+                </IonContent>
+            </IonPage >
+        )
+    }
 }
 
 export default Quiz;
