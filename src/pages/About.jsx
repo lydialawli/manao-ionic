4a29e9987b02384f07507266a16ba0366da7906e
Monica <monicarigi@hotmail.com>
2019-10-08 22:25:23 +0700

    about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { IonContent, IonHeader, IonIcon, IonItem, IonList, IonPage } from '@ionic/react';
-import { americanFootball, basketball, beer, bluetooth, boat, build, flask, football, paperPlane, wifi } from 'ionicons/icons';
+import { IonContent, IonHeader, IonPage, IonGrid, IonRow, IonCol, IonText } from '@ionic/react';
 import Toolbar from '../components/Toolbar.jsx'
+import '../styles/about.css'
 
 class About extends React.Component {
   render() {
@@ -10,43 +10,46 @@ class About extends React.Component {
         <IonHeader>
           <Toolbar />
         </IonHeader>
+				<IonContent className="main">
+					<img className="banner" alt="" src="/assets/banner.png"></img>
+	        <IonGrid className="aboutGrid">
+						<IonRow>
+							<IonCol>
+								<div style={{backgroundImage:'url(https://seakoala.io/docs/images/universeLy.png)'}} className="founderImg"></div>
+							</IonCol>
+							<IonCol>
+								<div style={{backgroundImage:'url(https://lh3.googleusercontent.com/uZsAOkhUMi_6QrWpbTIUNyb8HFTuhIIHAho2n0PeymiV184u3rXNu8AkLVh-P-U129kKZ5B_hhd5tnuE4RUYVpNRHZU=s195)'}} className="founderImg"></div>
+							</IonCol>
+						</IonRow>
+						<IonRow>
+							<IonCol>
+								<h1>Lydia</h1>
+							</IonCol>
+							<IonCol>
+								<h1>Monica</h1>
+							</IonCol>
+						</IonRow>
+						<IonRow>
+							<IonText className="about loc">
+								<i className="fas fa-map-marker-alt"></i> Amphoe Koh Sa-Mui, Surat Thani, Thailand
+							</IonText>
+						</IonRow>
+						<IonRow>
+							<IonText className="about phone">
+								<i className="fas fa-phone"></i> +66 98 785 3402
+							</IonText>
+						</IonRow>
+						<IonRow>
+							<IonText className="about mail">
+								<i className="fas fa-envelope"></i> manao@manao.com
+							</IonText>
+						</IonRow>
+					</IonGrid>
 
-        <IonContent>
-          <ListItems />
         </IonContent>
       </IonPage >
     )
   }
 }
-
-const ListItems = () => {
-  const icons = [
-    flask,
-    wifi,
-    beer,
-    football,
-    basketball,
-    paperPlane,
-    americanFootball,
-    boat,
-    bluetooth,
-    build
-  ];
-
-  const items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(x => {
-    return (
-      <IonItem key={x}>
-        <IonIcon icon={icons[x - 1]} slot="start" />
-        Item {x}
-        <div className="item-note" slot="end">
-          This is item # {x}
-        </div>
-      </IonItem>
-    );
-  });
-
-  return <IonList>{items}</IonList>;
-};
-
 
 export default About;
