611dd6cc9919f82ccc66b4bb3b7f6d03b7630e23
Monica <monicarigi@hotmail.com>
2019-10-08 21:23:48 +0700

    Join coming soon info

--- src/pages/Join.jsx
+++ src/pages/Join.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { IonContent, IonHeader, IonPage, IonButton } from '@ionic/react';
 import Toolbar from '../components/Toolbar.jsx'
-
+import '../styles/join.css'
 class Join extends React.Component {
 
   render() {
@@ -12,6 +12,10 @@ class Join extends React.Component {
         </IonHeader>
 
         <IonContent className="main ion-padding">
+					<div className="joinMessage">
+						<h1>Coming soon!</h1>
+						<h5>We are working hard to offer you the best multiplayer expericence!</h5>
+					</div>
           <IonButton className="play">JOIN GAME!</IonButton>
         </IonContent>
       </IonPage>
